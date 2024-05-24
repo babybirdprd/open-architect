cbc748843cbe3c574b2cea248625e9041ffc1754
babybirdprd <babybirdprd@gmail.com>
2024-05-24 01:27:48 -0400

    Your commit message

--- start_coding.py
+++ start_coding.py
@@ -3,29 +3,30 @@ from threading import Thread
 import os
 
 from src.helpers.github import GHHelper
-from src.helpers.trello import TrelloHelper
+# from src.helpers.trello import TrelloHelper
 
 from src.agents.intern import Intern
 from src.agents.reviewer import Reviewer
+from src.models import Ticket
 
 load_dotenv()
 
 gh_repo = os.getenv("GITHUB_REPO_URL")
 gh_api_token_intern = os.getenv("GITHUB_TOKEN_INTERN")
 gh_api_token_reviewer = os.getenv("GITHUB_TOKEN_REVIEWER")
-trello_api_key = os.getenv("TRELLO_API_KEY")
-trello_api_secret = os.getenv("TRELLO_API_SECRET")
-trello_token = os.getenv("TRELLO_TOKEN")
-trello_board_id = os.getenv("TRELLO_BOARD_ID")
+# trello_api_key = os.getenv("TRELLO_API_KEY")
+# trello_api_secret = os.getenv("TRELLO_API_SECRET")
+# trello_token = os.getenv("TRELLO_TOKEN")
+# trello_board_id = os.getenv("TRELLO_BOARD_ID")
 
 if (
     gh_repo is None
     or gh_api_token_intern is None
     or gh_api_token_reviewer is None
-    or trello_api_key is None
-    or trello_api_secret is None
-    or trello_token is None
-    or trello_board_id is None
+    # or trello_api_key is None
+    # or trello_api_secret is None
+    # or trello_token is None
+    # or trello_board_id is None
 ):
     print(
         "Please run the init_connections.py script to set up the environment variables"
@@ -33,13 +34,22 @@ if (
 
 gh_helper_intern = GHHelper(gh_api_token_intern, gh_repo)
 gh_helper_reviewer = GHHelper(gh_api_token_reviewer, gh_repo)
-trello_helper = TrelloHelper(trello_api_key, trello_token, trello_board_id)
+# trello_helper = TrelloHelper(trello_api_key, trello_token, trello_board_id)
 
-intern = Intern("alex", gh_helper=gh_helper_intern, board_helper=trello_helper)
+tasks = {
+            'Backlog': [],
+            'To Do': [],
+            'WIP': [],
+            'Ready for Review': [],
+            'Reviewed': [],
+            'Approved': []
+        }
+
+intern = Intern("alex", gh_helper=gh_helper_intern, tasks=tasks) #, board_helper=trello_helper)
 reviewer = Reviewer(
     "charlie",
     gh_helper=gh_helper_reviewer,
-    board_helper=trello_helper,
+    tasks=tasks, #, board_helper=trello_helper,
 )
 
 intern_thread = Thread(target=intern.run)
